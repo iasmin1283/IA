f1600334e6b113aa7db36483a58bae5168711e76
iasmin1283 <163189544+iasmin1283@users.noreply.github.com>
2024-12-03 08:30:58 -0300

    ...

--- main.js
+++ main.js
@@ -5,78 +5,108 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-    {
-        enunciado: "O cyberbullying é a violência virtual que ocorre geralmente com as pessoas tímidas e indefesas, ou simplesmente por não caírem na simpatia das mesmas. Como podemos evitá-lo",
-        alternativas: [
-            {
-                texto: "Utilizando palestras para prevenir o cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa reflexiva e busca aprendizado e conhecimento."
-            },
-            {
-                texto:  "Prestando atenção aos sinais de cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos."
-            }    
-           
-        ]
-    },
-    {
-       
-            enunciado: "As fontes renováveis de energia são aquelas formas de produção de energia em que suas fontes são capazes de manter-se disponíveis durante um longo prazo, contando com recursos que se regeneram ou que se mantêm ativos permanentemente. Com base nisso, temos a energia geotérmica, que consiste em utilizar o calor manifestado em áreas próximas à superfície. Qual a sua opinião sobre esse modelo de energia renovável?",
-            alternativas: [
-                {
-                    texto: "A energia geotérmica precisa ser mais explorada visto que utiliza o calor interno da terra, pois não depende do clima e é inesgotável.",
-                    afirmacao: "Você demonstra um perfil  de pesquisador e inovador que busca por meio da inovação soluções para o meio ambiente e sustentabilidade das gerações posteriores."
-                },
-                {
-                    texto:  "Seria interessante explorar primeiramente fontes renováveis exotérmicas, em segundo plano utilizar a energia geotérmica apesar dos custos elevados de explorações e possibilidades de contaminação de rios. ",
-                    afirmacao: "Você é uma pessoa que dá ênfase em priorizar os recursos disponíveis, pensando no meio ambiente e sem descartar o viés econômico."
-                }    
-               
-            ]
-        },
-        {
-            enunciado: "Hoje em dia é cada vez mais importante conhecer o seu corpo, suas qualidades, suas limitações e entender suas emoções. Saber controlá-las é o desafio das pessoas. A sua auto-estima impacta a sua qualidade de vida?",
-
-            alternativas: [
-                {
-                    texto: "Na maior parte do tempo, com certeza impacta, pois é a partir da autoestima que temos um olhar positivo ou negativo sobre nossas ações diárias. Sempre estou em busca de mais autoconhecimento.",
-                    afirmacao: "Você tem consciência que é importante ter uma boa auto-estima e procura se aprofundar mais sobre o assunto, vendo a vida de um jeito positivo."
-                },
-                {
-                    texto:    "Apenas ocasionalmente ou raramente. Às vezes minha autoestima oscila e isso pode afetar negativamente minha qualidade de vida em certas ocasiões.",
-               
-                    afirmacao: "Você demomostra que precisa se conhecer mais e mostra uma consciência de uma crescente necessidade de cuidar mais da própria percepção e bem-estar emocional."
-                }    
-               
-            ]
-        },
+{
+enunciado: "Quando você começa um novo projeto de vida, o que você gosta de fazer pra poder melhorar cada fez mais",
+alternativas: [
+{
+texto: "Gosto de planejar detalhadamente antes de começar a por em pratica.",
+afirmacao: "<b>Organização</b> é uma característica forte sua, valorizando o planejamento detalhado."
+},
+{
+texto: "Prefiro começar a pensar como vou fazer logo e ajustar o plano conforme avanço.",
+afirmacao: "Você se destaca pela <b>dinamismo</b>, gostando de aprender e ajustar durante o processo."
+}
 ]
-   
+},
+{
+enunciado: "Como você lida com problemas ou atrasos em sua rotina que é nescessaria?",
+alternativas: [
+{
+texto: "Gosto de resolver os problemas e tentar nao me atrasar mais para dar tempo de fazer tudo que preciso.",
+afirmacao: "Independência define sua abordagem para resolver problemas, confiando nas suas <b>habilidades</b>."
+},
+{
+texto: "Prefiro pedir ajuda para pessoas, para elas não deixar eu me atrasar ou me ajudar a fazer tudo na hora sem deixar para depois.",
+afirmacao: "Colaboração é essencial para você, apreciando a <b>troca de conhecimento</b> com outros colegas."
+}
+]
+},
+{
+enunciado: "Qual é a sua prefêrencia por esportes para sua saude fisica ?",
+alternativas: [
+{
+texto: "Acredito que a funcionalidade do esporte é melhor do que os beneficios.",
+afirmacao: "<b>volei</b> é um esporte que presica de muito esforco fisico e conhecimento sobre ele."
+},
+{
+texto: "Valorizo muito a estética e gosto de criar interfaces visualmente atraentes.",
+afirmacao: "<b>futebol</b>, futebol é um esporte que precisa de mais esforço que todos os outros esportes."
+}
+]
+},
+{
+enunciado: "Qual é o seu pensamento sobre todos os esportes para a saúde?",
+alternativas: [
+{
+texto: "Prefiro me aprofundar nas postagens e saber realmente para que serve os espertes.",
+afirmacao: "<b>Foco</b> é seu ponto forte, preferindo dominar suas ferramentas atuais."
+},
+{
+texto: "Gosto de experimentar e aprender novas tecnicas para meu funcionamento fisico",
+afirmacao: "<b>Curiosidade</b> é uma marca sua, sempre em busca de novas ferramentas e técnicas."
+}
+]
+},
+{
+enunciado: "Como você prefere fazer algumas coisas de exercício?",
+alternativas: [
+{
+texto: "Prefiro fazer coisas no modo sozinho e mais lento ( no meu ritmo).",
+afirmacao: "<b>Independência</b> é uma preferência sua, gostando de ter controle total sobre seus projetos."
+},
+{
+texto: "Gosto de trabalhar em equipe, colaborando e compartilhando responsabilidades.",
+afirmacao: "<b>Trabalho em equipe</b> é algo que você valoriza, apreciando a colaboração e a partilha de responsabilidades."
+}
+]
+}
+];
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta(){
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
+if(atual >= perguntas.length){
+mostraResultado();
+return;
 }
+perguntaAtual = perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+caixaAlternativas.textContent = "";
+mostraAlternativas();
+}
+
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
+for(const alternativa of perguntaAtual.alternativas){
+const botaoAlternativas = document.createElement("button");
+botaoAlternativas.textContent = alternativa.texto;
+botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+caixaAlternativas.appendChild(botaoAlternativas)
+}
 }
 
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacao = opcaoSelecionada.afirmacoes;
-    historiaFinal = afirmacao;
-    atual++;
-    mostraPergunta();
+const afirmacoes = opcaoSelecionada.afirmacao;
+historiaFinal += afirmacoes + " ";
+atual++;
+mostraPergunta();
 }
 
+function mostraResultado(){
+caixaPerguntas.textContent = "Olha só o que podemos afirmar sobre você...";
+textoResultado.innerHTML = historiaFinal;
+caixaAlternativas.innerHTML = "";
+}
 
 mostraPergunta();
